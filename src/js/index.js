b9f93ef446527bc1c80f354332c23bdc1af087c0
lugfigue <lugfigue@bancolombia.com.co>
2022-08-26 11:20:10 -0500

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,82 @@
-// Example of DOM Manipulation with Pure JavaScript
+let root = document.getElementById("root");
+let btnRock = document.getElementById("btn-rock");
+let bntPaper = document.getElementById("btn-paper");
+let bntScissors = document.getElementById("btn-scissors");
 
-const changeSubtextBackground = () => {
-  // document.querySelector() selects an HTML element from the page by its attribute
-    // ids are selected with the '#' preceeding the ID name
+// Escojer la accion del usuario ✅
+// Generar una jugada aleatoria de la maquina ✅
+// mostrar el resultado del juego ✅
 
-  // here we are selecting the <p id='subtext'></p> tag in our index.html
-  const subtext = document.querySelector('#subtext');
-  // this sets the 'style' attribute of the HTML element we selected
-  // the same as writing <p id='subtext' style="background-color: blue;"></p> in HTML
-  subtext.setAttribute('style', "background-color: blue" );
-}
+let rockAtion = () => {
+  let randomPlay = getRandomInt(3);
+  let result = genrateResult("rock", randomPlay);
+  alert(`${result} - ${translateNumberToAction(randomPlay)}`);
+};
 
-// calling a function from other.js
-// remember to import it into index.html and in the correct order of what is initiated
-exampleJavaScriptFn();
+let paperAction = () => {
+  let randomPlay = getRandomInt(3);
+  let result = genrateResult("paper", randomPlay);
+  alert(`${result} - ${translateNumberToAction(randomPlay)}`);
+};
 
+let scissorsAction = () => {
+  let randomPlay = getRandomInt(3);
+  let result = genrateResult("scissors", randomPlay);
+  alert(`${result} - ${translateNumberToAction(randomPlay)}`);
+};
 
-// now we call the function defined above
-// it will select the HTML element with id='subtext'
-// then it will color its background blue
-changeSubtextBackground();
+let getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
 
+let genrateResult = (userPlay, machinePlay) => {
+  if (userPlay === "paper") {
+    if (machinePlay === 0) {
+      return "Empate! 🤍";
+    }
+    if (machinePlay === 1) {
+      return "Pierdes! ❤️";
+    }
+    if (machinePlay === 2) {
+      return "Ganaste! 💚";
+    }
+  }
+  if (userPlay === "rock") {
+    if (machinePlay === 0) {
+      return "Pierdes! ❤️";
+    }
+    if (machinePlay === 1) {
+      return "Ganaste! 💚";
+    }
+    if (machinePlay === 2) {
+      return "Empate! 🤍";
+    }
+  }
+  if (userPlay === "scissors") {
+    if (machinePlay === 0) {
+      return "Ganaste! 💚";
+    }
+    if (machinePlay === 1) {
+      return "Empate! 🤍";
+    }
+    if (machinePlay === 2) {
+      return "Pierdes! ❤️";
+    }
+  }
+};
 
+let translateNumberToAction = (actionNumber) => {
+  if (actionNumber === 0) {
+    return "Papel!";
+  }
+  if (actionNumber === 1) {
+    return "Tijera!";
+  }
+  if (actionNumber === 2) {
+    return "Piedra!";
+  }
+};
+
+btnRock.addEventListener("click", rockAtion);
+bntPaper.addEventListener("click", paperAction);
+bntScissors.addEventListener("click", scissorsAction);
